725d64b8b306bdc56c6bc2804d7f3cf2349191ba
DimasFirjaIslam <dimasfirja80@gmail.com>
2024-09-18 02:49:19 -0700

    Add files via upload

---
+++ Kelas/Pertemuan2/pertemuan2.py
@@ -0,0 +1,77 @@
+#penulisan pertama snake case
+nama_mahasiswa = "ucup"
+
+#penulisan kedua camel case
+namaMahasiswa = "ucup"
+
+namaMahasiswaInformatika = "ucup2"
+
+#penulisan ketiga capitalize
+NamaMahasiswa = "ucup3"
+
+#penulisam variabel yang salah
+# 2mahasiswa = "2"
+
+#maha siswa = 22
+
+#if = 2
+#list = 2
+#for = 3
+
+#numerik int
+#number = 1
+#number2 = -1
+
+#float
+
+#number 3 = 1,2
+#number 4 = _1,5
+
+#string
+
+faklutas = "teknik"
+prodi = 'informatika'
+umur = "28"
+
+kalimat = """aku bisa"""
+banyak
+kata""""""
+
+kalimat = "aku bisa ngoding"
+print(kalimat 3:12)
+print(kalimat)
+
+print(kalimat[4])
+data = ["apel", 1, true]
+print(data[3][0])
+
+biodata = {
+    "nama" : "ucup"
+    "umur" : 25
+}
+
+print(biodata["nama"])
+
+print(biodata["umur"])
+nama = input("masukkkan nama : ")
+print(f"nama anda " + {nama}")
+
+#operator aritmatika
+print(1 + 2)
+print(3 - 2)
+print(6 * 8)
+
+#operator perbandingan
+print(2 > 3)
+
+KurUSD = 0.000065
+KurEUR = 0.00006
+KurYen = 0.0092\
+
+IDR = input ("Masukkan Harga Rupiah = ")
+
+USD = IDR * KurUSD
+EURO = IDR * KurEUR
+YEN = YEN * KurYen
+
+print(F'IDR = USD * KUREUR')
