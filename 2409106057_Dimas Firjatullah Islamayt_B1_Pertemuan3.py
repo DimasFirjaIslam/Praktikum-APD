24e90017ffa5fbf715be18449e49023ac3ba8c41
DimasFirjaIslam <dimasfirja80@gmail.com>
2024-09-19 23:56:10 -0700

    Add files via upload

---
+++ 2409106057_Dimas Firjatullah Islamayt_B1_Pertemuan3.py
@@ -0,0 +1,76 @@
+#Pertemuab 3
+
+cuaca = "cerah"
+if cuaca == "cerah":
+    print("kamu pergi keluar rumah")
+
+uang = "true"
+if uang:
+    print("Pergi ke pasar!")
+else:
+    print("Pergi ke rumah teman")
+
+#Variabel akan bernilai false jika string kosong 0 () [] {}, false
+#uang = ""
+#if uang:
+    #print ("Pergi ke pasar")
+#else:
+    #print ("Pergi ke rumah teman")
+
+nilai = 65
+if nilai < 70:
+    print("Tidak lulus")
+else:
+    print("lulus")
+
+bilangan = 3
+if bilangan % 2 == 0:
+    print("Bilangan genap")
+else:
+    print("Bilangan ganjil")
+
+Umur = int(input("Masukkan Umur Anda "))
+if Umur <= 10:
+    print("Anak - anak")
+elif Umur <= 18:
+    print("Remaja")
+elif Umur <=35:
+    print("Dewasa")
+elif Umur <=65:
+    print("Paruh baya")
+else:
+    print("Orang Tua")
+
+nilai = 79
+
+if nilai >= 80:
+    if nilai >= 85:
+        print("Nilai A+")
+    else :
+        print("Nilai A-")
+else :
+    print("Tidak Lulus")
+
+print("Menu :")
+print("1. Nonton Film")
+print("2. Ngoding")
+print("3. Keluar")
+menu = int(input("Pilih Menu Anda "))
+if menu == 1:
+    print("Nonton Film")
+elif menu == 2:
+    print("Ngoding")
+elif menu == 3:
+    print("Keluar")
+
+angka = 10
+if angka % 2 == 0:
+    print("Genap")
+if angka > 0:
+    print("Positif")
+
+if angka % 2 == 0:
+    print("genap")
+elif angka > 0:
+    print("Positif")
+
