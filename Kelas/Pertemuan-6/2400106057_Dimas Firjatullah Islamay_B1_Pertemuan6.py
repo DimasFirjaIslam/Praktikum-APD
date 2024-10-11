86c80e2129cd54514047fe46d5a7d4b11ca858ab
DimasFirjaIslam <dimasfirja80@gmail.com>
2024-10-11 14:55:05 +0800

    Add files via upload

---
+++ Kelas/Pertemuan-6/2400106057_Dimas Firjatullah Islamay_B1_Pertemuan6.py
@@ -0,0 +1,101 @@
+#data_mhs = {
+    #"nama": "ucup",
+    #"nim": 1,
+    #"matkul": ["APD", "Kalkulus", "Jarkom"]
+#}
+
+#for value in data_mhs.value()
+# print(value)
+
+#for key in data_mhs:
+    #print(data_mhs[key])
+
+#del data_mhs["nim"]
+
+#cache = data_mhs.pop("nim")
+
+#print(data_mhs, "Dictionary")
+#print(cache, "cache")
+
+#data_mhs["id"] = cache
+
+#print(data_mhs)
+
+#print(data_mhs.clear(), "clear")
+
+#print(len(data_mhs))
+
+#data_mhs['alamat'] = "Samarinda"
+#data_mhs['alamat'] = "Tenggarong"
+#print(data_mhs)
+
+#data_mhs.update({"alamat": "Samarinda"})
+#data_mhs.update({"alamat": "Tenggarong"})
+#print(data_mhs)
+
+#data_mhs["nama"] = 'Michael'
+
+#print(data_mhs.get('mapel, 'gak ada'))
+
+#for data in data_mhs:
+    #print(data)
+
+#for key_data, value_data in data_mhs.items():
+    #print(f"key: {key_data}\nValue: {value_data}\n")
+
+#print(data_mhs['mapel'])
+
+#print(data_mhs['nama'])
+
+#print(data_mhs['nim'])
+
+#print(data_mhs)
+
+#key = "Apel", "Jeruk", "Mangga", "Semangka", "Anggur"
+#value = 1
+#buah = dict.fromkeys(key, value)
+#print(buah)
+
+#Nilai = {
+    #Matematika" : 80,
+    #"B. Indonesia" : 90,
+    #"B. Inggris" : 81,
+    #"Kimia" : 20
+#}
+#sebelum Setdefault
+#print(Nilai)
+#rint("")
+#menggunakan setdefault
+#print("Nilai : ", Nilai.setdefault("Kimia", 70))
+#print("")
+#setelah menggunakan setdefault
+#print(Nilai)
+
+#data_mhs = {
+    #"nama" : "ucup",
+    #"nim" : 1,
+    #"matkul" : ["APD", "Kalkulus", "Jarkom"],
+    #"dosen" : {
+        #"nama" : "Pak Awang",
+        #"matkul": "APD"
+    #}
+#}
+
+#print(data_mhs["dosen'"]["nama"])
+
+#1,2,3 PTG
+#data_mhs = [
+    #{"nama" : "ucup",
+     #"role" : "admin",
+    #},
+
+    #{
+    #"nama" : "michael",
+    #"role" : "user",
+    #}
+#]
+
+#data_mhs2 = [
+    #["ucup", "admin"],
+    #["michael", "user"]
+#]
